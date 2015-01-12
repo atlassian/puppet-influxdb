a8914b08fbf2141f71668c6faa6dd18a58338f01
Matthew Moor <mmoor@atlassian.com>
2015-01-12 15:12:59 +1100

    First cut create_database defined type

---
+++ manifests/create_database.pp
@@ -0,0 +1,12 @@
+define influxdb::create_database(
+  uri = 'http://localhost:8086'
+  user = 'root',
+  password = 'root',
+) {
+
+  exec { "create database ${title}":
+    command => "curl -X POST '${uri}/db?u=${user}&p=${password}' -d '{\"name\": \"${title}\"}'",
+    unless  => "curl -s -X POST 'http://localhost:8086/db?u=${user}&p=${password}' -d '{\"name\": \"${title}\"}' | grep -q 'exists'"
+  }
+
+}
