4d2ed3dd858fa034a676f7482324e963e5617ca5
Otto Jongerius <ojongerius@atlassian.com>
2015-01-23 20:01:51 +1100

    Pull values from params.

--- manifests/create_database.pp
+++ manifests/create_database.pp
@@ -1,14 +1,15 @@
+# == Type: influxdb::create_database
 define influxdb::create_database (
-  $uri = 'http://localhost:8086',
-  $user = 'root',
-  $password = 'root',
+  $api_url        = "${influxdb::params::hostname}:${influxdb::params::api_port}/db",
+  $admin_user     = $influxdb::params::admin_user,
+  $admin_password = $influxdb::params::admin_password,
 ) {
 
   exec { "create database ${title}":
-    command   => "curl -s -X POST '${uri}/db?u=${user}&p=${password}' -d '{\"name\": \"${title}\"}'",
+    path      => '/bin:/usr/bin',
+    command   => "curl -s -X POST '${api_url}?u=${admin_user}&p=${admin_password}'  -d '{\"name\": \"${title}\"}'",
     tries     => 3,
     try_sleep => 3,
-    unless    => "curl -s -X POST 'http://localhost:8086/db?u=${user}&p=${password}' -d '{\"name\": \"${title}\"}' | grep -q 'exists'",
+    unless    => "curl -s -X POST '${api_url}?u=${admin_user}&p=${admin_password}' -d '{\"name\": \"${title}\"}' | grep -q 'exists'",
   }
-
 }
